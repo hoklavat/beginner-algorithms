2cefababf1a89b8b4f5d45a696a6e06ec4cbc90f
hoklavat <barisyildiz1982@gmail.com>
2021-01-05 14:26:31 +0300

    .

---
+++ MORE/Dynamic(UglyNumbers).cpp
@@ -0,0 +1,45 @@
+//Dynamic(UglyNumbers)
+//ugly numbers are numbers whose only prime factors are 2, 3 or 5. 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, ...
+//Task: find nth ugly number.
+//Reference: dynamic programming.
+//Time Complexity: O(n).
+
+#include <iostream>
+using namespace std;
+
+unsigned findUglyNumber(unsigned n){
+	unsigned ugly[n];
+	unsigned i2 = 0, i3 = 0, i5 = 0;
+	unsigned next_multiple_of_2 = 2;
+	unsigned next_multiple_of_3 = 3;
+	unsigned next_multiple_of_5 = 5;
+	unsigned next_ugly_no = 1;
+
+	ugly[0] = 1; //first ugly number.
+	for(int i = 1; i < n; i++){
+		next_ugly_no = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5));
+		ugly[i] = next_ugly_no;
+		if(next_ugly_no == next_multiple_of_2){
+			i2 = i2+1;
+			next_multiple_of_2 = ugly[i2]*2;
+		}
+		if(next_ugly_no == next_multiple_of_3){
+			i3 = i3+1;
+			next_multiple_of_3 = ugly[i3]*3;
+		}
+		if(next_ugly_no == next_multiple_of_5){
+			i5 = i5+1;
+			next_multiple_of_5 = ugly[i5]*5;
+		}
+	}
+	
+	return next_ugly_no;
+}
+
+int main(){
+	int n = 150;
+	
+	cout << findUglyNumber(n) << endl;
+	
+	return 0;
+}
