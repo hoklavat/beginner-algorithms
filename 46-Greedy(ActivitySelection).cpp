ed76ced1874424ea6a712d0a4527bbe65370a257
hoklavat <barisyildiz1982@gmail.com>
2021-01-18 15:35:02 +0300

    .

--- 46-Greedy(ActivitySelection).cpp
+++ 46-Greedy(ActivitySelection).cpp
@@ -1,17 +1,23 @@
 //46-Greedy(ActivitySelection)
+//Task: n activities are given with their start and finish times. 
+//select maximum number of activities that can be performed by single person, 
+//assuming that person can only work on single activity at time.
+//Reference: greedy.
+//Time Complexity: O(nlogn) if unsorted. O(n) if sorted.
 
 #include<iostream>
 using namespace std;
 
-void printMaxActivities(int s[], int f[], int n){ //start time, finish time, number of activities.	
+void print(int S[], int F[], int n){ //start time, finish time, number of activities.	
 
 	cout << "Following activities are selected: ";
 	
-	int i = 0, j;
-	cout << i << " "; //first activity.
+	int i = 0;
+	int j = 0;
+	cout << i << " "; //first activity. activities are sorted by their finish times.
 
-	for (j = 1; j < n; j++){
-		if (s[j] >= f[i]){ //finish time is greater than previous activity start time.
+	for(j = 1; j < n; j++){
+		if (S[j] >= F[i]){ //finish time is greater than previous activity start time.
 			cout << j << " ";
 			i = j;
 		}
@@ -21,10 +27,8 @@ void printMaxActivities(int s[], int f[], int n){ //start time, finish time, num
 
 int main(){
 	int s[] = {1, 3, 0, 5, 8, 5};
-	int f[] = {2, 4, 6, 7, 9, 9}; //activities are already sorted by their finish times.
-	int n = sizeof(s)/sizeof(s[0]);
-	
-	printMaxActivities(s, f, n);
-	
+	int f[] = {2, 4, 6, 7, 9, 9};
+	int n = sizeof(s)/sizeof(s[0]);	
+	print(s, f, n);	
 	return 0;
 }
